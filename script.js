875f1947f84d85a5853073c17e72ac8621a97d63
Kevin Nguyen <darkeavin@outlook.com>
2024-03-18 22:30:03 -0400

    categorie et type de document

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 var listeLivre = [
     {
-"titre" : "Harry Potter à l'école des sorciers",
-"urlImage" : "https://m.media-amazon.com/images/I/516qWQcG3FL.jpg",
-"categorie" : "Romans"
-},
-{
+    "titre" : "Harry Potter à l'école des sorciers",
+    "urlImage" : "https://m.media-amazon.com/images/I/516qWQcG3FL.jpg",
+    "categorie" : "Romans"
+    },
+    {
     "titre" : "Descartes",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwGyB9GoEk4AvaLeC5u37KyozJBbWayiEnTxtCK9xf&s",
     "categorie" : "Philosophie"
     }]
 
-var listeCategorie = [ "Albums","Bande dessinée", "Contes", "Documentaire", "Journaux", "Magazines", "Mangas",
-"Nouvelles",
-"Philosophie",
-"Poésie",
-"Romans"]
+var listeType = ["Albums","Bande dessinée", "Contes", "Documentaire", "Journaux",
+                        "Magazines", "Mangas", "Nouvelles", "Philosophie", "Poésie", "Romans"]
+
+var listeCategorie = ["Aventure", "Biographie", "Fantastique", "Histoire", "Policier", "Romance", "Science-Fiction"]
 
 const afficherLivre = function(listeLivre) {
     listeLivre.forEach((livre) => {
@@ -31,10 +30,25 @@ const afficherLivre = function(listeLivre) {
     })
 }
 
+const afficherType = function(listeType) {
+    listeType.forEach((type) => {
+        const parent = document.querySelector('nav')
+        const balisteListe = parent.querySelector('#filtreType');
+        const baliseItem = document.createElement('li');
+        const baliseNom = document.createElement('a');
+
+        baliseNom.textContent = type;
+        baliseNom.href = "?"+type;
+        
+        baliseItem.append(baliseNom);
+        balisteListe.append(baliseItem);
+    })
+}
+
 const afficherCategorie = function(listeCategorie) {
     listeCategorie.forEach((categorie) => {
         const parent = document.querySelector('nav')
-        const balisteListe = parent.querySelector('ul');
+        const balisteListe = parent.querySelector('#filtreCategorie');
         const baliseItem = document.createElement('li');
         const baliseNom = document.createElement('a');
 
@@ -43,9 +57,9 @@ const afficherCategorie = function(listeCategorie) {
         
         baliseItem.append(baliseNom);
         balisteListe.append(baliseItem);
-        parent.appendChild(balisteListe);
     })
 }
 
 afficherLivre(listeLivre);
+afficherType(listeType);
 afficherCategorie(listeCategorie);
