239f6fb1b59160569e972d3f0b25b2d8326d42c5
Kevin Nguyen <darkeavin@outlook.com>
2024-03-19 12:45:08 -0400

    barre de recherche

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var listeLivre = [
     {
     "titre" : "Harry Potter à l'école des sorciers",
+    "auteur" : "J. K. Rowling",
     "urlImage" : "https://m.media-amazon.com/images/I/516qWQcG3FL.jpg",
     "type" : "Romans",
     "categorie" : "Fantastique"
     },
     {
     "titre" : "Tintin au Congo",
+    "auteur" : "Hergé",
     "urlImage" : "https://m.media-amazon.com/images/I/61Ergcdo5NL._AC_UF1000,1000_QL80_.jpg",
     "type" : "Bande dessinée",
     "categorie" : "Aventure"
@@ -14,48 +16,56 @@ var listeLivre = [
     },
     {
     "titre" : "Le Seigneur des Anneaux : La communauté de l'Anneau",
+    "auteur" : "John Ronald Reuel Tolkien",
     "urlImage" : "https://www.gallimardmontreal.com/system/articles/images/grand/9782266107983.jpg",
     "type" : "Romans",
     "categorie" : "Fantastique"
     },
     {
     "titre" : "Le Petit Prince",
+    "auteur" : "Antoine de Saint-Exupéry",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRUmdhsYTRKcUZIWeZalepg8Hv4m03wMHnWA&usqp=CAU",
     "type" : "Romans",
     "categorie" : "Enfant"
     },
     {
     "titre" : "Orgueil et Préjugés",
+    "auteur" : "Jane Austen",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSE7To1NYv9vIQDXe3Py2Ji4B-d0J9J8R12w&usqp=CAU",
     "type" : "Romans",
     "categorie" : "Romance"
     },
     {
     "titre" : "1984",
+    "auteur" : "Geroge Orwell",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsc3GiO3O4clyHm3TfXypE3U1K9vlLZDYw1g&usqp=CAU",
     "type" : "Romans",
     "categorie" : "Science-Fiction"
     },
     {
     "titre" : "Le Hobbit",
+    "auteur" : "John Ronald Reuel Tolkien",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSB2yqM8df3Mq8ZCPx51mYM3YUVbIYU2PDVbCzYoSP23HmcLwyJboblvw3xl481o7HHxQ&usqp=CAU",
     "type" : "Romans",
     "categorie" : "Fantastique"
     },
     {
     "titre" : "Les Misérables",
+    "auteur" : "Victor Hugo",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2Jkwp31A1LQ7U-cXfEUuMGFDBbsP3l6AxUw&usqp=CAU",
     "type" : "Romans",
     "categorie" : "Aventure"
     },
     {
     "titre" : "L'Alchimiste",
+    "auteur" : "Paulo Coelho",
     "urlImage" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT16SUZ0VJPaN0NDp0_L3EsM7tu1_asO0JWrA&usqp=CAU",
     "type" : "Philosophie",
     "categorie" : "Aventure"
     },
     {
     "titre" : "Discours de la méthode",
+    "auteur" : "René Descartes",
     "urlImage" : "https://m.media-amazon.com/images/I/61njB87x6-L._SY425_.jpg",
     "type" : "Essais",
     "categorie" : "Philosophie"
@@ -146,6 +156,24 @@ const afficherCategorie = function(listeCategorie) {
         baliseItem.append(baliseBtn);
         balisteListe.append(baliseItem);
     })
+}
+
+const rechercheSoumettre = document.getElementById('submitRecherche');
+const rechercheMot = document.getElementById("recherche");
+rechercheSoumettre.onclick = function() {
+    const motCle = rechercheMot.value.toUpperCase();
+    console.log(motCle);
+    var filtreType = [];
+    var article = document.querySelectorAll('article');
+    article.forEach((item) => {
+        item.remove();
+    })
+    listeLivre.forEach((livre) => {
+        if(livre.titre.toUpperCase().indexOf(motCle) > -1 || livre.auteur.toUpperCase().indexOf(motCle) > -1) {
+            filtreType.push(livre);
+        }
+    })
+    afficherLivre(filtreType);
 }
 
 afficherLivre(listeLivre);
