089d776a11750cf207b918f1f9cb5320a02ad57b
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-02 16:00:04 -0400

    Quand aucun livre

--- api/livres/getLivresCatType.php
+++ api/livres/getLivresCatType.php
@@ -5,7 +5,7 @@ $sql = "SELECT *
         FROM Livre
         JOIN Type_Livre ON Livre.ISBN = Type_Livre.isbn_livre 
         JOIN Categorie_Livre ON Livre.ISBN = Categorie_Livre.isbn_livre
-        WHERE (Categorie_Livre.id_categorie = :categorie OR :categorie = 0) AND (Type_Livre.id_type = :type OR :type = 0)";
+        WHERE (Categorie_Livre.id_categorie = :categorie OR $categorie = 0) AND (Type_Livre.id_type = :type OR $type = 0)";
 
 $stmt = $pdo->prepare($sql);
 
@@ -25,7 +25,7 @@ if ($livres) {
     echo json_encode($livres);
     exit;
 } else {
-    header("HTTP/1.0 404 Not Found");
+    echo json_encode("");
     exit;
 }
 ?>
