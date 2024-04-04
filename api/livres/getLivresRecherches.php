0b9d05371128819b448d2296cd1957efe3bb6509
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 13:53:38 -0400

    parametre

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -10,8 +10,7 @@ if(isset($motCle)){
     OR UPPER(Auteur.nom) LIKE UPPER(:motCle) 
     OR UPPER(Auteur.prenom) LIKE UPPER(:motCle) 
     OR UPPER(Livre.isbn) LIKE UPPER(:motCle))");
-    $stmt->bindParam(":motCle", $motCleParam);
-
+    $stmt->bindParam(":motCle", $motCleParam, PDO::PARAM_STR);
     $stmt->execute();
 
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
