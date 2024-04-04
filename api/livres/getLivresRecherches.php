5ec16446fa57201d7cc250ab880a4a9433b37bb8
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 14:01:03 -0400

    motCle

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -2,14 +2,14 @@
 require_once __DIR__."/../../config.php";
 
 if(isset($motCle)){
-    $motCle = '%$motCle%';
+    
     $stmt = $pdo->prepare("SELECT * FROM Livre 
     LEFT OUTER JOIN Auteur ON Livre.id_auteur = Auteur.id
-    WHERE (UPPER(Livre.titre) LIKE UPPER(:motCle)
-    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(:motCle) 
-    OR UPPER(Auteur.nom) LIKE UPPER(:motCle) 
-    OR UPPER(Auteur.prenom) LIKE UPPER(:motCle) 
-    OR UPPER(Livre.isbn) LIKE UPPER(:motCle))");
+    WHERE (UPPER(Livre.titre) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    OR UPPER(Auteur.nom) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    OR UPPER(Auteur.prenom) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    OR UPPER(Livre.isbn) LIKE UPPER(CONCAT('%',:motCle,'%'))");
     $stmt->bindParam(":motCle", $motCle, PDO::PARAM_STR);
     $stmt->execute();
 
