7e53f2a34bbd7e8b0a57d879df7a69e76dbf9eda
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 13:47:30 -0400

    concatenation

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -3,15 +3,15 @@ require_once __DIR__."/../../config.php";
 
 
 if(isset($motCle)){
-    $motCleParam = "%$motCle%";
+    
     $stmt = $pdo->prepare("SELECT * FROM Livre 
     left outer JOIN Auteur on Livre.id_auteur=Auteur.id
-    WHERE (upper(Livre.titre) like upper($motCleParam)
-    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER($motCleParam) 
-    OR upper(Auteur.nom) like upper($motCleParam) 
-    OR upper(Auteur.prenom) like upper($motCleParam) 
-    OR upper(Livre.isbn) like upper($motCleParam))");
-    $stmt->bindParam(":motCle", $motCleParam);
+    WHERE (upper(Livre.titre) like upper('%'||$motCle||'%')
+    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom) LIKE UPPER('%'||$motCle||'%') 
+    OR upper(Auteur.nom) like upper('%'||$motCle||'%') 
+    OR upper(Auteur.prenom) like upper('%'||$motCle||'%') 
+    OR upper(Livre.isbn) like upper('%'||$motCle||'%'))");
+    $stmt->bindParam(":motCle", '%'||$motCle||'%');
     
   
     $stmt->execute();
