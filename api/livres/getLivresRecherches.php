7ffcd8aae7d2133978919a421b5faf78f952874d
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 12:55:42 -0400

    recherche

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -3,25 +3,20 @@ require_once __DIR__."/../../config.php";
 
 $motCle = isset($_GET['motCle']) ? $_GET['motCle'] : null;
 
-if($motCle !== null ){
-    
-    $stmt = $pdo->prepare("SELECT * FROM `Livre` WHERE UPPER(Livre.titre) LIKE ?");
-    
-    $motCle = '%' . $motCle . '%';
-    
-    $stmt->bindValue(1, $motCle, PDO::PARAM_STR);
+if(isset($motCle) && filter_var($motCle, FILTER_VALIDATE_INT)){
+    $stmt = $pdo->prepare("SELECT * FROM `Livre` 
+    WHERE upper(Livre.titre) like upper('%'||$motCle||'%')");
+    $stmt->bindParam(":motCle", $motCle);
     $stmt->execute();
 
-    $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    // Vérifiez si aucun livre correspondant n'a été trouvé
-    if (empty($livres)) {
-        $livres = ["error" => "Aucun livre trouvé pour le mot-clé spécifié"];
-    }
+    $livre = $stmt->fetch();
 } else {
-    $livres = ["error" => "Mot-clé invalide"];
+    $livre = ["error"=>"invalide"];
 }
 
-// Envoyer la réponse en JSON
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($livres);
+
+if($livre){
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($livre);
+    exit;
+}
