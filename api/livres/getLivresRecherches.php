2842b31fb3e354e0531aa91a749e02c71007afe3
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 13:10:59 -0400

    motCle

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -1,34 +1,21 @@
 <?php
-require_once __DIR__ . "/../../config.php";
+require_once __DIR__."/../../config.php";
 
-$motCle = isset($_GET['motCle']) ? $_GET['motCle'] : null;
-
-$response = [];
-
-if ($motCle !== null) {
-
-    $sql = "SELECT * FROM `Livre` WHERE UPPER(titre) LIKE :motCle";
-    $stmt = $pdo->prepare($sql);
-    $motCleParam = '%' . strtoupper($motCle) . '%'; 
-    $stmt->bindParam(':motCle', $motCleParam, PDO::PARAM_STR);
+if(isset($motCle)){
+    $stmt = $pdo->prepare("SELECT * FROM `Livre` 
     
-    // Exécuter la requête
-    if ($stmt->execute()) {
-        $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        if (!empty($livres)) {
-            $response = ["livres" => $livres];
-        } else {
-            $response = ["message" => "Aucun livre trouvé pour le mot-clé spécifié"];
-        }
-    } else {
-        $response = ["error" => "Erreur lors de l'exécution de la requête SQL"];
-    }
+    WHERE upper(Titre) like upper('%'||$motCle||'%');");
+    $stmt->bindParam(":motCle", $motCle);
+    $stmt->execute();
+
+    $livre = $stmt->fetch();
 } else {
-    $response = ["error" => "Paramètre de recherche 'motCle' non spécifié dans l'URL"];
+    $livre = ["error"=>"invalide"];
 }
 
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($response);
-exit;
-?>
+
+if($livre){
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($livre);
+    exit;
+}
