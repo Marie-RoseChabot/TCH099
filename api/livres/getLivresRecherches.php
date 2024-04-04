f65c5f17d40d179b7fd63cf4023e94c1fc7a0b2d
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 14:08:25 -0400

    fonctionne?

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -5,11 +5,11 @@ if(isset($motCle)){
     
     $stmt = $pdo->prepare("SELECT * FROM Livre 
     LEFT OUTER JOIN Auteur ON Livre.id_auteur = Auteur.id
-    WHERE (UPPER(Livre.titre) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Auteur.nom) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Auteur.prenom) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Livre.isbn) LIKE UPPER(CONCAT('%',:motCle,'%')))");
+    WHERE (UPPER(Livre.titre) LIKE UPPER(CONCAT('%',:motCle,'%'))");
+    //OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    //OR UPPER(Auteur.nom) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    //OR UPPER(Auteur.prenom) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    //OR UPPER(Livre.isbn) LIKE UPPER(CONCAT('%',:motCle,'%')))");
     $stmt->bindParam(":motCle", $motCle, PDO::PARAM_STR);
     $stmt->execute();
 
