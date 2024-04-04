bd466595db7d72d5d74365e0b6a1c1c2554e0232
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 13:51:15 -0400

    bindParam

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -1,19 +1,20 @@
 <?php
 require_once __DIR__."/../../config.php";
 
-
 if(isset($motCle)){
-    
+    $motCleParam = "%$motCle%";
     $stmt = $pdo->prepare("SELECT * FROM Livre 
-    left outer JOIN Auteur on Livre.id_auteur=Auteur.id
-    WHERE (upper(Livre.titre) like upper('%'||$motCle||'%')
-    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom) LIKE UPPER('%'||$motCle||'%') 
-    OR upper(Auteur.nom) like upper('%'||$motCle||'%') 
-    OR upper(Auteur.prenom) like upper('%'||$motCle||'%') 
-    OR upper(Livre.isbn) like upper('%'||$motCle||'%'))");
-    $stmt->bindParam(":motCle", '%'||$motCle||'%');
-    
-  
+    LEFT OUTER JOIN Auteur ON Livre.id_auteur = Auteur.id
+    WHERE (UPPER(Livre.titre) LIKE UPPER(:motCle)
+    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(:motCle) 
+    OR UPPER(Auteur.nom) LIKE UPPER(:motCle) 
+    OR UPPER(Auteur.prenom) LIKE UPPER(:motCle) 
+    OR UPPER(Livre.isbn) LIKE UPPER(:motCle))");
+    $stmt->bindParam(":motCle", $motCleParam);
+    $stmt->bindParam(":motCle", $motCleParam);
+    $stmt->bindParam(":motCle", $motCleParam);
+    $stmt->bindParam(":motCle", $motCleParam);
+    $stmt->bindParam(":motCle", $motCleParam);
     $stmt->execute();
 
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
