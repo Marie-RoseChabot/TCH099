d5a1f81eb53de0681273ee9b030ded67e6ad11fc
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 13:28:48 -0400

    motCle

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -1,14 +1,13 @@
 <?php
 require_once __DIR__."/../../config.php";
 
-$motCle = isset($_GET['motCle']) ? $_GET['motCle'] : null;
+
 
 if(isset($motCle)){
+    $motCleParam = "%$motCle%";
     $stmt = $pdo->prepare("SELECT * FROM Livre 
     WHERE upper(titre) like upper(:motCle)");
-
-    $motCleParam = "%$motCle%";
-    $stmt->bindParam(":motCle", $motCle);
+    $stmt->bindParam(":motCle", $motCleParam);
     $stmt->execute();
 
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -18,3 +17,4 @@ if(isset($motCle)){
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($livres);
+?>
