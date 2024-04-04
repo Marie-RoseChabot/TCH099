e073434f40394e2d6ae818688f3e817b3214d796
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 14:11:50 -0400

    fonctionne?

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -1,23 +1,20 @@
 <?php
 require_once __DIR__."/../../config.php";
 
+
+
 if(isset($motCle)){
-    
-    $stmt = $pdo->prepare("SELECT * FROM Livre 
-    LEFT OUTER JOIN Auteur ON Livre.id_auteur = Auteur.id
-    WHERE (UPPER(Livre.titre) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Auteur.nom) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Auteur.prenom) LIKE UPPER(CONCAT('%',:motCle,'%'))
-    OR UPPER(Livre.isbn) LIKE UPPER(CONCAT('%',:motCle,'%')))");
-    $stmt->bindParam(":motCle", $motCle, PDO::PARAM_STR);
+    $stmt = $pdo->prepare("SELECT * FROM Livre ");
+    //WHERE upper(titre) like upper(:motCle)");
+
+    $motCleParam = "%$motCle%";
+    $stmt->bindParam(":motCle", $motCleParam);
     $stmt->execute();
 
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } else {
-    $livres = ["error" => "invalid"];
+    $livres = ["error" => "invalide"];
 }
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($livres);
-?>
