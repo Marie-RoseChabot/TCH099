7b7c49d01de274884715cfde5812bc8c78910ce3
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 14:27:55 -0400

    essaie

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -5,13 +5,20 @@ require_once __DIR__."/../../config.php";
 if(isset($motCle)){
     
     $stmt = $pdo->prepare("SELECT * FROM Livre 
-    
-    WHERE (upper(Livre.titre) like upper(CONCAT('%',:motCle,'%')))
-    OR (upper(Livre.titre) like upper(CONCAT('%',:motCle2,'%')))");
+    LEFT OUTER JOIN Auteur ON Livre.id_auteur = Auteur.id
+    WHERE (UPPER(Livre.titre) LIKE UPPER(CONCAT('%',:motCle,'%'))
+    OR UPPER(CONCAT(Auteur.prenom, ' ', Auteur.nom)) LIKE UPPER(CONCAT('%',:motCle2,'%'))
+    OR UPPER(Auteur.nom) LIKE UPPER(CONCAT('%',:motCle3,'%'))
+    OR UPPER(Auteur.prenom) LIKE UPPER(CONCAT('%',:motCle4,'%'))
+    OR UPPER(Livre.isbn) LIKE UPPER(CONCAT('%',:motCle5,'%')))");
 
-    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+
+    
     $stmt->bindParam(":motCle", $motCle,PDO::PARAM_STR);
     $stmt->bindParam(":motCle2", $motCle,PDO::PARAM_STR);
+    $stmt->bindParam(":motCle3", $motCle,PDO::PARAM_STR);
+    $stmt->bindParam(":motCle4", $motCle,PDO::PARAM_STR);
+    $stmt->bindParam(":motCle5", $motCle,PDO::PARAM_STR);
 
     $stmt->execute();
 
