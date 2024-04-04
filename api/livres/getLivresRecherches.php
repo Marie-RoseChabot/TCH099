b1d702815b60f2f49765d53d665d4c1e9e415b68
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-04 12:50:19 -0400

    recherche

--- api/livres/getLivresRecherches.php
+++ api/livres/getLivresRecherches.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__."/../../config.php";
 
-
 $motCle = isset($_GET['motCle']) ? $_GET['motCle'] : null;
 
 if($motCle !== null ){
@@ -10,14 +9,19 @@ if($motCle !== null ){
     
     $motCle = '%' . $motCle . '%';
     
-    
     $stmt->bindValue(1, $motCle, PDO::PARAM_STR);
     $stmt->execute();
 
     $livres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Vérifiez si aucun livre correspondant n'a été trouvé
+    if (empty($livres)) {
+        $livres = ["error" => "Aucun livre trouvé pour le mot-clé spécifié"];
+    }
 } else {
-    $livres = ["error" => "invalid keyword"];
+    $livres = ["error" => "Mot-clé invalide"];
 }
 
+// Envoyer la réponse en JSON
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($livres);
