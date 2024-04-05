c798151c64f3a9678ce3667c26716020d5ebb55b
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-05 18:57:04 -0400

    config

--- config.php
+++ config.php
@@ -3,8 +3,6 @@
 // Démarrage de la session
 session_start();
 
-session_start();
-
 require_once __DIR__."/vendor/php-jwt/JWTExceptionWithPayloadInterface.php";
 require_once __DIR__."/vendor/php-jwt/BeforeValidException.php";
 require_once __DIR__."/vendor/php-jwt/CachedKeySet.php";
@@ -13,6 +11,7 @@ require_once __DIR__."/vendor/php-jwt/JWK.php";
 require_once __DIR__."/vendor/php-jwt/JWT.php";
 require_once __DIR__."/vendor/php-jwt/Key.php";
 require_once __DIR__."/vendor/php-jwt/SignatureInvalidException.php";
+
 
 global $API_SECRET;
 $API_SECRET = "MaCleSecrete123!"; // Utilisez une clé secrète forte
