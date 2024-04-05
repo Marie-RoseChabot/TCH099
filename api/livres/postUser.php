240a74521faa948fd748737759c84a8c90514fce
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-05 17:34:41 -0400

    login

--- api/livres/postUser.php
+++ api/livres/postUser.php
@@ -1,31 +1,33 @@
 <?php
 require_once __DIR__."/../../config.php";
-$message = "";
+$message="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $prenom = $_POST['prenom'];
-    $nom = $_POST['nom'];
-    $courriel = $_POST['courriel'];
-    $dateNaissance = $_POST['dateNaissance'];
-    $username = $_POST['username'];
-    $password = $_POST['motDePasse'];
-    $typeUsager = $_POST['typeCompte'];
-    $confirmPass = $_POST['confirmerMdp'];
-
-    // Hasher le mot de passe
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-    // Préparer la requête SQL
-    $stmt = $pdo->prepare('INSERT INTO Usager (`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`) 
-                            VALUES (?, ?, ?, ?, ?, ?, ?)');
+    $prenom = $_POST['prenom'] ?? '';
+    $nom = $_POST['nom'] ?? '';
+    $courriel = $_POST['courriel'] ?? '';
+    $dateNaissance = $_POST['dateNaissance'] ?? '';
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['motDePasse'] ?? '';
+    $typeUsager = $_POST['typeCompte'] ?? '';
+    $confirmPass = $_POST['confirmerMdp'] ?? '';
     
-    // Exécuter la requête SQL
-    if ($stmt->execute([$username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, 'client'])) {
-        header("Location: login.php");
-        exit;
+    // Vérifier si toutes les données sont fournies
+    if (!empty($prenom) && !empty($nom) && !empty($courriel) && !empty($dateNaissance) && !empty($username) && !empty($password) && !empty($typeUsager)) {
+        
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $pdo->prepare('INSERT INTO Usager (`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`) VALUES (?, ?, ?, ?, ?, ?, ?)');
+        if ($stmt->execute([$username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, $typeUsager])) {
+            header("Location: login.php");
+            exit;
+        } else {
+            $message = 'Erreur lors de la création du compte.';
+        }
     } else {
-        $message = 'Erreur lors de la création du compte.';
+        $message = 'Veuillez fournir toutes les données requises.';
     }
+} else {
+    $message = 'La méthode de requête n\'est pas valide.';
 }
 
 echo $message;
