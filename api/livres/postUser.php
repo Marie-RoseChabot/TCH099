942588166704c26670a6eaedee9d059025d3f861
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-05 14:26:26 -0400

    User

--- api/livres/postUser.php
+++ api/livres/postUser.php
@@ -1,32 +1,24 @@
 <?php
 require_once __DIR__."/../../config.php";
 
-if (!isset($_SERVER["CONTENT_TYPE"]) || $_SERVER["CONTENT_TYPE"] != 'application/json') {
-    http_response_code(400);
-    exit;
-}
+$prenom = $_POST['prenom'];
+  $nom = $_POST['nom'];
+  $courriel = $_POST['courriel'];
+  $dateNaissance = $_POST['dateNaissance'];
+  $username = $_POST['username'];
+  $password = $_POST['motDePasse'];
+  $typeUsager = $_POST['typeCompte'];
+  $confirmPass = $_POST['confirmerMdp'];
 
-$body = json_decode(file_get_contents("php://input"));
+ // Hasher le mot de passe
+ $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-try {
-    $stmt = $pdo->prepare("INSERT INTO `Usager`(`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`)
-     VALUES (:username, :mdp, :courriel, :nom, :prenom, :dateNaissance, 'client')");
-
-    $stmt->bindParam(':username', $body->username);
-    $stmt->bindParam(':mdp', $body->password);
-    $stmt->bindParam(':courriel', $body->courriel);
-    $stmt->bindParam(':nom', $body->nom);
-    $stmt->bindParam(':prenom', $body->prenom);
-    $stmt->bindParam(':dateNaissance', $body->dateNaissance);
-
-    $stmt->execute();
-
-    
-
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode(["success" => true]);
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(["error" => "Erreur lors de l'insertion en BD: " . $e->getMessage()]);
-}
+ // Insérer le nouvel utilisateur
+ $stmt = $pdo->prepare('INSERT INTO Usager (`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`) VALUES (?, ?, ?, ?, ?, ?, ?)');
+ if ($stmt->execute([$username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, $typeUsager])) {
+     header("Location: login.php");
+     exit;
+ } else {
+     $message = 'Erreur lors de la création du compte.';
+ }
 ?>
