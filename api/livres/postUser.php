68683dc19fd2fdd17c1e5b0efc7d67a4afe5eccf
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-05 17:30:49 -0400

    register

--- api/livres/postUser.php
+++ api/livres/postUser.php
@@ -1,27 +1,32 @@
 <?php
 require_once __DIR__."/../../config.php";
-$message="";
+$message = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$prenom = $_POST['prenom'];
-  $nom = $_POST['nom'];
-  $courriel = $_POST['courriel'];
-  $dateNaissance = $_POST['dateNaissance'];
-  $username = $_POST['username'];
-  $password = $_POST['motDePasse'];
-  $typeUsager = $_POST['typeCompte'];
-  $confirmPass = $_POST['confirmerMdp'];
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $courriel = $_POST['courriel'];
+    $dateNaissance = $_POST['dateNaissance'];
+    $username = $_POST['username'];
+    $password = $_POST['motDePasse'];
+    $typeUsager = $_POST['typeCompte'];
+    $confirmPass = $_POST['confirmerMdp'];
 
- $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    // Hasher le mot de passe
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-
- $stmt = $pdo->prepare('INSERT INTO Usager (`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`) 
- VALUES ($username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, 'client')');
- if ($stmt->execute([$username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, $typeUsager])) {
-     header("Location: login.php");
-     exit;
- } else {
-     $message = 'Erreur lors de la création du compte.';
- }
- echo $message;
+    // Préparer la requête SQL
+    $stmt = $pdo->prepare('INSERT INTO Usager (`username`, `password`, `courriel`, `nom`, `prenom`, `date_naissance`, `type_usager`) 
+                            VALUES (?, ?, ?, ?, ?, ?, ?)');
+    
+    // Exécuter la requête SQL
+    if ($stmt->execute([$username, $passwordHash, $courriel, $nom, $prenom, $dateNaissance, 'client'])) {
+        header("Location: login.php");
+        exit;
+    } else {
+        $message = 'Erreur lors de la création du compte.';
+    }
 }
+
+echo $message;
 ?>
