ce5cea3c0d34fa90b6e395d5bec5d675749cef52
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-05 19:20:32 -0400

    C:\Users\Dell\Documents\Uni\Bac\Session4\projet\Web\TCH099\vendor\php_jwt

--- config.php
+++ config.php
@@ -2,7 +2,16 @@
 
 // Démarrage de la session
 session_start();
-require_once __DIR__ . "/vendor/autoload.php";
+
+require_once __DIR__."C:\Users\Dell\Documents\Uni\Bac\Session4\projet\Web\TCH099\vendor\php_jwt/JWTExceptionWithPayloadInterface.php";
+require_once __DIR__."/vendor/php-jwt/BeforeValidException.php";
+require_once __DIR__."/vendor/php-jwt/CachedKeySet.php";
+require_once __DIR__."/vendor/php-jwt/ExpiredException.php";
+require_once __DIR__."/vendor/php-jwt/JWK.php";
+require_once __DIR__."/vendor/php-jwt/JWT.php";
+require_once __DIR__."/vendor/php-jwt/Key.php";
+require_once __DIR__."/vendor/php-jwt/SignatureInvalidException.php";
+
 
 global $API_SECRET;
 $API_SECRET = "MaCleSecrete123!"; 
