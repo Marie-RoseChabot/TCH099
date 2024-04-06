8ffa925faef5d223e77b565a42dad87084b6cd2c
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-06 17:30:45 -0400

    getCopie

---
+++ api/livres/getCopie.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__."/../../config.php";
+
+if(isset($isbn) ){
+    $stmt = $pdo->prepare("SELECT COUNT(id_copie) as count FROM `Copie` WHERE `isbn_livre`=:isbn"
+    && `est_dispo`=1);
+    $stmt->bindParam(":isbn", $isbn);
+    $stmt->execute();
+
+    $copieCount = $stmt->fetch(PDO::FETCH_ASSOC);
+    $count = $copieCount['count'];
+} else {
+    $count = 0; 
+}
+
+
+echo json_encode([$count]);
