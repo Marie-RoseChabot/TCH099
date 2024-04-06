871f65c0b28f79934820a0ba8e951f774455dfdf
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-06 15:02:59 -0400

    critiques

--- api/livres/getCritiques.php
+++ api/livres/getCritiques.php
@@ -1,16 +1,23 @@
 <?php
 require_once __DIR__."/../../config.php";
-
-if (isset($isbn)) {
+if(isset($isbn) && filter_var($isbn, FILTER_VALIDATE_INT)){
+    $stmt = $pdo->prepare("SELECT * FROM `Critique` WHERE `isbn`=:isbn");
     $stmt->bindParam(":isbn", $isbn);
+    $stmt->execute();
+
+    $critiques = $stmt->fetch();
+} else {
+    $critiques = ["error"=>"Code ISBN invalide"];
 }
-$stmt = $pdo->prepare('SELECT * FROM Critique WHERE `isbn`=:isbn');
-$stmt->execute();
 
 
-$critiques = $stmt->fetchAll();
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($critiques);
+if($critiques){
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($livre);
+    exit;
+} else {
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}
 
 ?>
