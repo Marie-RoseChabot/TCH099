be3046c16690e027d41055b130f56c09c777db0e
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-06 16:35:18 -0400

    signalement

---
+++ api/livres/putSignalement.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__."/../../config.php";
+
+if(isset([$isbn])) {
+    
+
+    $stmt = $pdo->prepare("UPDATE Critique SET est_signale = 'Oui' WHERE isbn = :isbn");
+    $stmt->bindValue(":isbn", $isbn);
+    
+    if ($stmt->execute()) {
+        echo json_encode(array('success' => true, 'message' => 'Signalement status updated successfully'));
+    } else {
+        echo json_encode(array('success' => false, 'message' => 'Failed to update signalement status'));
+    }
+} else {
+    echo json_encode(array('success' => false, 'message' => 'ISBN not provided'));
+}
+?>
--- routes.php
+++ routes.php
@@ -21,6 +21,7 @@ get('/api/email/$email','/api/livres/getNbEmail.php');
 get('/api/critique/$isbn','/api/livres/getCritiques.php');
 get('/api/user/$username','/api/livres/getNbUser.php');
 put('/api/livres/$isbn', '/api/livres/putLivres.php');
+put('/api/signalement/$isbn','/api/livres/putSignalement.php');
 post('/api/livres', '/api/livres/postLivre.php');
 post('/api/usager', '/api/livres/postUser.php');
 post('/api/auth','/api/livres/auth.php');
