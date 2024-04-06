93e0466e996a77002ddcd86af7bce5f728643318
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-06 15:00:45 -0400

    bind avant

--- api/livres/getCritiques.php
+++ api/livres/getCritiques.php
@@ -1,11 +1,12 @@
 <?php
 require_once __DIR__."/../../config.php";
 
-$stmt = $pdo->prepare('SELECT * FROM Critique WHERE `isbn`=:isbn');
-$stmt->execute();
 if (isset($isbn)) {
     $stmt->bindParam(":isbn", $isbn);
 }
+$stmt = $pdo->prepare('SELECT * FROM Critique WHERE `isbn`=:isbn');
+$stmt->execute();
+
 
 $critiques = $stmt->fetchAll();
 
