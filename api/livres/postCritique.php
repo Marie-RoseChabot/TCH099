d54078ffc10be00d1aed35b1e57e7761c6c2de0b
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-08 16:21:03 -0400

    ;

--- api/livres/postCritique.php
+++ api/livres/postCritique.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once __DIR__."/../../config.php";
+require_once __DIR__ . "/../../config.php";
 
 $userid = 0;
 
 try {
     $userid = authentifier();
-} catch(Exception $e) {
+} catch (Exception $e) {
     $response = [];
     http_response_code(401);
     $response['error'] = "Non autorisé";
@@ -16,21 +16,20 @@ try {
 
 $body = json_decode(file_get_contents("php://input"));
 
-try{
+try {
     $stmt = $pdo->prepare("INSERT INTO `Critique` 
     (`etoiles`, `commentaire`, `est_signale`, `username_client`, `isbn`) 
-    VALUES (:isbn, :commentaire, 'Non',':user,Livre.isbn_livre)
-    LEFT OUTER JOIN Livre on Livre.titre=:titre");
+    VALUES (:etoiles, :commentaire, 'Non', :user, (SELECT Livre.isbn_livre FROM Livre WHERE Livre.titre = :titre))");
     $stmt->bindValue(":etoiles", $body->note);
-    $stmt->bindValue(":titre",$body->titre);
-    $stmt->bindValue(":commentaire",$body->critique)
-    $stmt->bindValue(":user",$userid)
+    $stmt->bindValue(":titre", $body->titre);
+    $stmt->bindValue(":commentaire", $body->critique);
+    $stmt->bindValue(":user", $userid);
     $stmt->execute();
 
-   // $insertion = [];
-    //header('Content-Type: application/json; charset=utf-8');
-    //echo json_encode($insertion);
-} catch (PDOException $e){
+    // $insertion = [];
+    // header('Content-Type: application/json; charset=utf-8');
+    // echo json_encode($insertion);
+} catch (PDOException $e) {
     http_response_code(500);
-    echo "Erreur lors de l'insertion en BD: ".$e->getMessage();
+    echo "Erreur lors de l'insertion en BD: " . $e->getMessage();
 }
