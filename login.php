418bee6fcce24fbd38fc62e1016a146d7cac4fdf
Julhemond <julien.hemond.1@ens.etsmtl.ca>
2024-04-08 09:41:54 -0400

    modifs css

--- login.php
+++ login.php
@@ -37,12 +37,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Saisir le lien du css ici -->
+    <link rel="stylesheet" href="style.css" />
     <title>Connexion</title>
   </head>
   <body>
     <main>
-      <form id="formLogin" class="formulaires" action="./login.php" method="post">
+      <form id="formLogin" class="formulairesAuthentification" action="./login.php" method="post">
         <h2>Identifiez-vous</h2>
         <!-- Si l'utilisateur entre son email, ça devrait quand même fonctionner -->
         <label for="username">Nom d'utilisateur : </label>
