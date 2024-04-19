83d3e457b19f6698311038be74a1d87b524d8ab2
Marie-Rose <marierose.chabot@hotmail.com>
2024-04-19 14:27:17 -0400

    id_type

--- api/livres/postType.php
+++ api/livres/postType.php
@@ -8,21 +8,17 @@ if(!isset($_SERVER["CONTENT_TYPE"]) || $_SERVER["CONTENT_TYPE"]!='application/js
 
 $body = json_decode(file_get_contents("php://input"));
 
-
-
-
-
-try{
-    $stmt = $pdo->prepare("INSERT INTO `Type_Livre` (`isbn_livre`,`id_type` ) VALUES (:isbn, :id_type)");
+try {
+    $stmt = $pdo->prepare("INSERT INTO `Type_Livre` (`isbn_livre`, `id_type`) VALUES (:isbn, :id_type)");
     $stmt->bindValue(":isbn", $body->isbn);
-    $stmt->bindValue(":id_type", $body->id);
+    $stmt->bindValue(":id_type", $body->id); 
     $stmt->execute();
 
-    $insertion = ["isbn"=>$body->isbn, "id_type"=>$id_type];
+    $insertion = ["isbn" => $body->isbn, "id_type" => $body->id];
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($insertion);
-}
-catch (PDOException $e){
+} catch (PDOException $e) {
     http_response_code(500);
-    echo "Erreur lors de l'insertion en BD: ".$e->getMessage();
+    echo "Erreur lors de l'insertion en BD: " . $e->getMessage();
 }
+?>
